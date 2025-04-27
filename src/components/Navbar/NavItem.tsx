54649c53f0af7fb4fe20da45442f2b4ea8e763aa
Miguel Raposo <migpovrap@hotmail.com>
2025-04-27 23:20:54 +0100

    feat(navbar-mobile): new navbar for mobile devices and general refactor

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -4,11 +4,12 @@ import styles from './Navbar.module.css';
 type NavItemProps = {
   href: string;
   label: string;
+  active: boolean;
 };
 
-export default function NavItem({ href, label }: NavItemProps) {
+export default function NavItem({ href, label, active }: NavItemProps) {
   return (
-    <Link href={href} className={styles.navLink} >
+    <Link href={href}  className={`${styles.navLink} ${active ? styles.active : ''}`} >
       {label}
     </Link>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,64 @@
+"use client"
+
 import NavItem from './NavItem';
 import styles from "./Navbar.module.css";
 import { GiFox } from "react-icons/gi";
 import { ThemeToggle } from './ThemeToggle';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { Divide as Hamburger } from 'hamburger-react'
 
-export default function Navbar() {
+function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 769);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+ console.log(menuOpen);
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${menuOpen ? styles.menuOpen : ''}`}>
       <nav className={styles.nav}>
-        <GiFox className={styles.navIcon}/>
-        <NavItem href="/" label="Home" />
-        <NavItem href="/projects" label="Projects" />
-        <NavItem href="/about" label="About" />
+        <GiFox className={styles.navIcon} />
+        
+        {!isMobile ? (
+            <div className={styles.navLinks}>
+            <NavItem href="/" label="Home" active={pathName === "/"} />
+            <NavItem href="/projects" label="Projects" active={pathName === "/projects"} />
+            <NavItem href="/about" label="About" active={pathName === "/about"} />
+            </div>
+        ) : (
+          <div className={styles.mobileIcons}>
+            <button onClick={toggleMenu} className={styles.menuButton}>
+              <Hamburger size={23} rounded hideOutline={true} />
+            </button>
+          </div>
+        )}
+
         <ThemeToggle />
       </nav>
+      {isMobile && menuOpen && (
+        <div className={styles.mobileMenu}>
+          <NavItem href="/" label="Home" active={pathName === "/"} />
+          <NavItem href="/projects" label="Projects" active={pathName === "/projects"} />
+          <NavItem href="/about" label="About" active={pathName === "/about"} />
+        </div>
+      )}
     </header>
   );
 }
+
+export default Navbar;
